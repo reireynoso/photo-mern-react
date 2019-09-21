d6ec52b695d560a4ed2e30895fccaa76c992e7f7
reireynoso <reireynoso@gmail.com>
2019-09-20 22:15:53 -0400

    nav-bar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,40 @@ class App extends Component{
     return (
     <div className="App">
         <NavBar/>
-        
+        <div id="pic-container" className="ui container">
+        <div class="ui three column grid">
+  <div class="column">
+    <div class="ui fluid card">
+      <div class="image">
+        <img src="/images/avatar/large/daniel.jpg"/>>
+      </div>
+      <div class="content">
+        <a class="header">Daniel Louise</a>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui fluid card">
+      <div class="image">
+        <img src="/images/avatar/large/helen.jpg"/>>
+      </div>
+      <div class="content">
+        <a class="header">Helen Troy</a>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui fluid card">
+      <div class="image">
+        <img src="/images/avatar/large/elliot.jpg"/>>
+      </div>
+      <div class="content">
+        <a class="header">Elliot Fu</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
     </div>
     )
   } 
